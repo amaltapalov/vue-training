c70dcadf7a8e9db140a3b6c991f96cabf5bb8598
Amal Tapalov <amaltapalov@gmail.com>
2019-09-24 16:54:26 +0200

    Add Employee Table component

--- src/App.vue
+++ src/App.vue
@@ -11,16 +11,19 @@
     <FlipCard />
     <hr />
     <MyAccordion />
+    <hr />
+    <EmployeeTable v-bind:employees="employees" />
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld";
-import Book from "./components/Book";
-import List from "./components/List";
-import Accordion from "./components/Accordion";
-import FlipCard from "./components/FlipCard";
-import MyAccordion from "./components/MyAccordion";
+import HelloWorld from "@/components/HelloWorld";
+import Book from "@/components/Book";
+import List from "@/components/List";
+import Accordion from "@/components/Accordion";
+import FlipCard from "@/components/FlipCard";
+import MyAccordion from "@/components/MyAccordion";
+import EmployeeTable from "@/components/EmployeeTable";
 
 export default {
   name: "App",
@@ -30,7 +33,29 @@ export default {
     List,
     Accordion,
     FlipCard,
-    MyAccordion
+    MyAccordion,
+    EmployeeTable
+  },
+  data() {
+    return {
+      employees: [
+        {
+          id: 1,
+          name: "Richard Hendricks",
+          email: "richard@piedpiper.com"
+        },
+        {
+          id: 2,
+          name: "Bertram Gilfoyle",
+          email: "gilfoyle@piedpiper.com"
+        },
+        {
+          id: 3,
+          name: "Dinesh Chugtai",
+          email: "dinesh@piedpiper.com"
+        }
+      ]
+    };
   }
 };
 </script>
---
+++ src/components/EmployeeTable.vue
@@ -0,0 +1,46 @@
+<template>
+  <div id="employee-table">
+    <EmployeeForm @add:employee="addEmployee" />
+    <table>
+      <thead>
+        <tr>
+          <th>Employee name</th>
+          <th>Employee email</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="employee in employees" :key="employee.id">
+          <td>{{ employee.name }}</td>
+          <td>{{ employee.email }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+import EmployeeForm from "./EmployeeForm";
+export default {
+  name: "EmployeeTable",
+  components: {
+    EmployeeForm
+  },
+  props: {
+    employees: Array
+  },
+  methods: {
+    addEmployee: function(employee) {
+      const lastId =
+        this.employees.length > 0
+          ? this.employees[this.employees.length - 1].id
+          : 0;
+      const id = lastId + 1;
+      const newEmployee = { ...employee, id };
+
+      this.employees = [...this.employees, employee];
+    }
+  }
+};
+</script>
+
+<style scoped></style>
